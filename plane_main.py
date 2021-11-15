435cc339507c4598ae93cea1338674882af754e0
冰糖雪狸 <46406730+NekoSilverFox@users.noreply.github.com>
2021-11-16 02:47:48 +0300

    修复 main文件中的错误

--- plane_main.py
+++ plane_main.py
@@ -5,14 +5,19 @@
     3. 启动游戏
 """
 
-class PlaneGame(object):
+class PlaneGame():
+    # 初始化
     def __init__(self):
         print("游戏初始化...")
 
+    # 开始游戏
     def start_game(self):
         print("游戏开始...")
 
-        
+    # 事件监听
+    def __event_handler(self):
+        pass
 
+# 【程序入口】 主函数
 if __name__ == '__main__':
     print("游戏初始化...")
