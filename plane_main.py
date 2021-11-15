0421e71156f9a7cd87815316f2c8667c63015abd
冰糖雪狸 <46406730+NekoSilverFox@users.noreply.github.com>
2021-11-16 02:25:25 +0300

    增加 注释说明

--- plane_main.py
+++ plane_main.py
@@ -1,1 +1,14 @@
-class PlaneGame():
+"""
+游戏主程序
+    1. 封装 主游戏类
+    2. 创建 游戏对象
+    3. 启动游戏
+"""
+
+class PlaneGame(object):
+    def __init__(self):
+        print("游戏初始化...")
+
+
+if __name__ == '__main__':
+    print("游戏初始化...")
